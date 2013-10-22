c86ef58e681f1966e98f50c7e9882acf88819ac4
Will Ballard <wballard@mailframe.net>
2013-10-22 12:04:07 -0500

    updated to have a socket io self reference

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
+app.get('/.test', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 app.get('/*', function(request, response) {
   response.end(request.path.slice(1));
 });
+io.sockets.on('connection', function (socket) {
+  socket.on('echo', function (data) {
+    socket.emit('echo', data);
+    console.log(data);
+  });
+});
 var port = process.env.PORT || 3000;
-app.listen(port);
+server.listen(port);
 console.log('Listening on ' + port);
